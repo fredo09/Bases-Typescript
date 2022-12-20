daf72a0c0f2a6974a55a5142bbd6facb11b2cc2e
josealfredo.vazquez.contractor <josealfredo.vazquez.contractor@bbva.com>
2022-12-20 15:29:30 -0600

    Usando y entendiendo el tipo Any

---
+++ Bases/tipos/any.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    const exists;
+    let power;
+    avenger = 'DR STRANGE';
+    console.log(avenger.charAt(0));
+})();
---
+++ Bases/tipos/any.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    let avenger:any = 123;
+    const exists;
+    let power;
+
+    avenger = 'DR STRANGE';
+
+    console.log(avenger.charAt(0));
+
+})()
