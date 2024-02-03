79d7452928acc51cf2c860c8c459364b42c6c36e
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2024-02-03 00:18:31 -0600

    hemos iniciado con la seccion de tipos de funciones y sus variantes en typescript

---
+++ Bases/funciones/funtions-type.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ *  Funciones de tipo
+ */
+(() => {
+    const addNumbers = (a, b) => a + b;
+    const greet = (name) => `Hola ${name}`;
+    const saveWord = () => 'El mundo esta salvado...';
+    // Asignar una variable como tipo Function
+    // let myFunction: Function;
+    //Indicamos que la variable sera de tipo funcion y regresa un number y con sus parametros
+    //let myFunction: (x: number, y: number) => number;
+    //Indicamos que la variable sera de tipo funcion y regresa un number con sus parametros
+    //let myFunction: (str: string) => string;
+    //indicamos que la varible sera de tipo funcion y no regresa string
+    let myFunction;
+    //* myFunction = 10;
+    //* console.log("🚀 ~ myFunction 1 :", myFunction);
+    //* myFunction = addNumbers;
+    //* console.log("🚀 ~ myFunction 2 ~ addNumbers :", myFunction(1, 2));
+    //? console.log("🚀 ~ myFunction 3 ~ greet:", myFunction('Memo'));
+    //? myFunction = greet;
+    myFunction = saveWord;
+    console.log("🚀 ~ myFunction 4 ~ saveWord:", myFunction());
+})();
